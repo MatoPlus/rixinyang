f9a3af74119bb453841787b4f4cce7bd9da963ad
MatoPlus <rixin.yang.2001@gmail.com>
2020-08-02 11:39:31 -0400

    Removed unnecessary div

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,13 +3,11 @@ import React from "react"
 const Footer = () => (
   <footer>
     <div className="content-grid">
-      <div>
-        Made with <span className="highlight">React</span> and{" "}
-        <span className="highlight">Gatsby</span>{" "}
-        <span role="img" aria-label="heart">
-          ❤
-        </span>
-      </div>
+      Made with <span className="highlight">React</span> and{" "}
+      <span className="highlight">Gatsby</span>{" "}
+      <span role="img" aria-label="heart">
+        ❤
+      </span>
     </div>
   </footer>
 )
